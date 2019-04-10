81e10d2b3df3c27b9ee34ab462bc86f4a579afd9
Afaq Tariq <afaq@superformula.com>
2019-04-10 15:20:48 -0500

    add a simple prod-only change order event dangerfile

---
+++ dangerfile.js
@@ -0,0 +1,46 @@
+/**
+ * @name Production PR Dangerfile
+ *
+ * @description
+ * This dangerfile validates production PRs before the deployment step
+ * to assure all change order requirements are met.
+ * The data captured here is then forwarded to Azure.
+ *
+ * See the following orb commands for reference:
+ * validate-change-management-event-info
+ * send-change-management-event
+ */
+const { danger, warn, message } = require('danger');
+const { checkAssignees } = require('danger-plugin-complete-pr');
+// const deploymentSummarySectionTitle = '# Production Deployment Summary';
+const deploymentSummarySectionTitle = '## Checklist';
+const prodChangeEvent = {
+  requestor: null,
+  templateId: null,
+  affectedEndUser: null,
+  assignee: 'MGMResorts/prod-deployment-approvers',
+  scheduledStartDate: new Date(),
+};
+
+// Requestor Field
+checkAssignees();
+prodChangeEvent.requestor = danger.github.pr.assignee.login;
+
+// Deployment Summary
+checkDeploymentSummary();
+
+function checkDeploymentSummary() {
+  if (danger.github.pr.base.ref !== 'prod') return;
+
+  const hasDeploymentSummarySection = danger.github.pr.body.includes(deploymentSummarySectionTitle)
+  const hasDeploymentSummaryText = danger.github.pr.body.split(deploymentSummarySectionTitle).length
+    && danger.github.pr.body.split(deploymentSummarySectionTitle)[1].length > 7
+
+  if (!hasDeploymentSummarySection || !hasDeploymentSummaryText) {
+    fail('Deployment Summary section is missing. Add "# Production Deployment Summary" to the end of the PR description followed by changes in this deployment.');
+  } else {
+    prodChangeEvent.deploymentSummaryText = danger.github.pr.body.split(deploymentSummarySectionTitle)[1];
+    message(`Azure change management system event: ${JSON.stringify(prodChangeEvent)}`);
+  }
+}
+
