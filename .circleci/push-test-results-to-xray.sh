58224a4e215dbcb0a7a350b61d4c7eeb20429b82
codin-sf <codin@superformula.com>
2019-06-26 20:33:09 -0700

    Updated a few items in script

--- .circleci/push-test-results-to-xray.sh
+++ .circleci/push-test-results-to-xray.sh
@@ -2,26 +2,16 @@
 
 set -e
 
-
-XRAY_CLIENT_ID=4BB10706536D4C5FA3B37E2828116857
-XRAY_CLIENT_SECRET=2a299a2d18aff586a52156f298880b823dd2272648e3b8e9d69bce190b80f056
-
-
+echo "Push Xray Version 1.0"
 echo "File to process: $1"
 echo "Jira Project Name: $2"
 
 SERVICE_DIR=$(pwd)
 echo "Project Path: $SERVICE_DIR"
-
-CIRCLE_SHA1=${2:-$(cd $SERVICE_DIR && git rev-parse HEAD)}
-
-# if ! test -f $SERVICE_DIR/package.json; then
-#   echo "SERVICE package.json not found"
-#   exit 1
-# fi
-
-# Mocha Results
+CIRCLE_SHA1=$(cd $SERVICE_DIR && git rev-parse HEAD)
+echo "CIRCLE_SHA1 = $CIRCLE_SHA1"
 TEST_RESULTS_FILE=$1
+echo $TEST_RESULTS_FILE
 
 if ! test -f $TEST_RESULTS_FILE; then
   echo "mocha test results file not found ($TEST_RESULTS_FILE)"
@@ -34,14 +24,6 @@ XRAY_AUTH_TOKEN=$(\
     --data "{ \"client_id\": \"$XRAY_CLIENT_ID\", \"client_secret\": \"$XRAY_CLIENT_SECRET\" }" \
     | tr -d '"'
 )
-
-echo "CIRCLE_SHA1 = $CIRCLE_SHA1"
-echo "XRAY_AUTH_TOKEN = $XRAY_AUTH_TOKEN"
-
-echo "https://xray.cloud.xpand-it.com/api/v1/import/execution/junit?projectKey=$2&revision=$CIRCLE_SHA1"
-echo $TEST_RESULTS_FILE
-
-
 
 RESULTS=$(\curl "https://xray.cloud.xpand-it.com/api/v1/import/execution/junit?projectKey=$2&revision=$CIRCLE_SHA1" \
   -H "Content-Type: text/xml" \
