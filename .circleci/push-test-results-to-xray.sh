ac5245fad18db7f0c3d07e177028f06620432875
codin-sf <codin@superformula.com>
2019-06-26 21:23:41 -0700

    Added Cucumber into the mix.

--- .circleci/push-test-results-to-xray.sh
+++ .circleci/push-test-results-to-xray.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
+
+#usage bash ./.circleci/push-test-results-to-xray.sh /pth/to/test-results.xml JIRAPROJECTCODE cucumber
 set -e
 
-echo "Push Xray Version 1.0"
+echo "Push Xray Version 1.0.1"
 echo "File to process: $1"
 echo "Jira Project Name: $2"
+echo "Test Type: $3"
 
 SERVICE_DIR=$(pwd)
-echo "Project Path: $SERVICE_DIR"
 CIRCLE_SHA1=$(cd $SERVICE_DIR && git rev-parse HEAD)
-echo "CIRCLE_SHA1 = $CIRCLE_SHA1"
 TEST_RESULTS_FILE=$1
+PROJECT_CODE=$2
+TEST_TYPE=$3
+
+echo "CIRCLE_SHA1 = $CIRCLE_SHA1"
+echo "Project Path: $SERVICE_DIR"
 echo $TEST_RESULTS_FILE
 
 if ! test -f $TEST_RESULTS_FILE; then
@@ -25,11 +31,24 @@ XRAY_AUTH_TOKEN=$(\
     | tr -d '"'
 )
 
-RESULTS=$(\curl "https://xray.cloud.xpand-it.com/api/v1/import/execution/junit?projectKey=$2&revision=$CIRCLE_SHA1" \
-  -H "Content-Type: text/xml" \
-  -X POST \
-  -H "Authorization: Bearer $XRAY_AUTH_TOKEN" \
-  --data @"$TEST_RESULTS_FILE"
-)
+# Supporting 2 file type API upload methods
+if [$TEST_TYPE = 'cucumber']; then
+
+  RESULTS=$(\curl "https://xray.cloud.xpand-it.com/api/v1/import/execution/cucumber?projectKey=$PROJECT_CODE&revision=$CIRCLE_SHA1" \
+    -H "Content-Type: application/json" \
+    -X POST \
+    -H "Authorization: Bearer $XRAY_AUTH_TOKEN" \
+    --data @"$TEST_RESULTS_FILE"
+  )
+else
+
+  RESULTS=$(\curl "https://xray.cloud.xpand-it.com/api/v1/import/execution/junit?projectKey=$PROJECT_CODE&revision=$CIRCLE_SHA1" \
+    -H "Content-Type: text/xml" \
+    -X POST \
+    -H "Authorization: Bearer $XRAY_AUTH_TOKEN" \
+    --data @"$TEST_RESULTS_FILE"
+  )
+fi
+
 
 echo "UPLOAD RESULT = $RESULTS"
